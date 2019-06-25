384f50e5ec4e76fab4f2cef04c4d70c8260d576c
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-06-25 19:12:40 +0200

    Add Demo App to GitOps manifests

---
+++ manifests/01_Demo_App/01_deploy_demo_app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 1. Monitor results (run in separate terminal):
+watch -n 1 "kubectl get all -n demo"
+
+# 2. Apply the manifests:
+kubectl apply -f ./
+
+# 3. See the results (demo service):
+URL=$(minikube service --url -n demo demo)
+kubectl get pod -Lapp/name -L app/instance -L app/version
+curl $URL -s
