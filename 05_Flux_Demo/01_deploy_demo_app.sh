58cff5e30625e87ab82b94dae4ab033468f9ac3f
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-06-25 10:10:47 +0200

    Improve script: 05_Flux_Demo/01_deploy_demo_app.sh

--- 05_Flux_Demo/01_deploy_demo_app.sh
+++ 05_Flux_Demo/01_deploy_demo_app.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# 1. Monitor results (run in separate terminal):
+watch -n 1 "kubectl get all -n demo"
+
+# 2. Copy manifests to /manifests dir
 rsync -av ../01_Demo_App/ ../manifests/01_Demo_App/
+git diff --stat
+
+# 3. Commit the changes to GIT:
+git add ../manifests/01_Demo_App/
+git commit --message="Add Demo App to GitOps manifests"
+
+# 4. Push to github and observe Flux logs:
+git push origin master
+kubectl logs -n flux deployment/flux --since 10m --follow | egrep demo
