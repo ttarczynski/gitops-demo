67a0d07eeeec5f8321400a9e3c7a1c384c4a5e1c
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-06-25 14:51:17 +0200

    Add the Helm Operator Demo

---
+++ 09_Helm_Operator_Demo/01_release_the_demo_app.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 1. Monitor results (run in separate terminal):
+  URL=$(minikube service --url -n demo demo); watch -n 1 "kubectl get namespace demo; echo; kubectl get pod -Lapp/name -L app/instance -L app/version; echo; curl $URL -s"
+
+# 2. Copy namespace and HelmRelease to /manifests
+  cp 01_namespace.yaml 02_helm_release.yaml ../manifests/
+  git status
+
+# 3. Commit the changes to GIT:
+  git add ../manifests/
+  git commit --message="Release the demo app using Helm"
+
+# 4. Push to github and observe Flux logs:
+  git push origin master
+  kubectl logs -n flux deployment/flux --since 10m --follow | egrep demo
+
+# 5. See Flux Helm Operator logs:
+  kubectl logs deployment/flux-helm-operator -n flux --since 5m  --follow | egrep demo
+
+# 6. See Helm Releases
+  helm ls
