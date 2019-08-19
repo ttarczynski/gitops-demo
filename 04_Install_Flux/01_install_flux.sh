02339fe93d05a00cfa96bd5497f271e26a8cbfb6
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-08-19 11:34:38 +0200

    Updates in 01_install_flux.sh and enable 'syncGarbageCollection'

--- 04_Install_Flux/01_install_flux.sh
+++ 04_Install_Flux/01_install_flux.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# 1. Add the Weave Flux chart repo:
+# 1. Add the Flux chart repository:
 
-helm repo add weaveworks https://weaveworks.github.io/flux
+helm repo add fluxcd https://charts.fluxcd.io
 
 # 2. Install Weave Flux:
 
 helm upgrade --install flux \
 --namespace flux \
 --values ./values_flux.yaml \
-weaveworks/flux
+fluxcd/flux
