9e0d954590b24828521ce27e680e35654c3eefed
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-08-19 09:01:39 +0200

    Add 'minikube start' to 01_install_helm.sh

--- 03_Install_Helm/01_install_helm.sh
+++ 03_Install_Helm/01_install_helm.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-# 1. Install the Helm Client
+# 0. Istall minikube
+#
+#   Follow the instructions at: https://kubernetes.io/docs/tasks/tools/install-minikube/
+
+# 1. Start minikube
+
+minikube start
+
+# 2. Install the Helm Client
 
 curl -LO https://git.io/get_helm.sh
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-# 2. Install Tiller
+# 3. Install Tiller
 
 kubectl -n kube-system create sa tiller
 
