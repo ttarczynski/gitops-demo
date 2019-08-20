0737a774373bb7bd4e0a3f6afa01a452bcd61664
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-08-20 08:19:44 +0200

    Improvements in 06_Cleanup/01_cleanup.sh

--- 06_Cleanup/01_cleanup.sh
+++ 06_Cleanup/01_cleanup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # 1. Monitor results (run in separate terminal):
-  URL=$(minikube service --url -n demo demo); watch -n 1 "kubectl get namespace demo; echo; kubectl get pod -Lapp/name -L app/instance -L app/version; echo; curl $URL -s"
+  URL=$(minikube service --url -n demo demo | sed 's/\* //');
+  watch -n 1 "kubectl get namespace demo; echo; kubectl get pod -n demo -Lapp/name -L app/instance -L app/version;
+  echo '----';
+  curl $URL -s"
 
 # 2. Remove manifests from GIT:
   rm -rf ../manifests/01_Demo_App/
@@ -13,7 +16,4 @@
 
 # 4. Push to github and observe Flux logs:
   git push origin master
-  kubectl logs -n flux deployment/flux --since 10m --follow | egrep demo
-
-# 5. Remove the demo namespace
-  kubectl delete namespaces demo --wait=false
+  kubectl logs -n flux deployment/flux --since 10m --follow | egrep 'deleting|deleted|delete'
