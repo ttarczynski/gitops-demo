250e408203afc1b6c4e405eaf1458f7aa1ac15b1
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-09-05 16:49:41 +0200

    Changes in 09_Helm_Operator_Demo/04_delete_release.sh

--- 09_Helm_Operator_Demo/04_delete_release.sh
+++ 09_Helm_Operator_Demo/04_delete_release.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # 1. Monitor results (run in separate terminal):
-  URL=$(minikube service --url -n demo demo); watch -n 1 "kubectl get pod -Lapp/name -L app/instance -L app/version; echo; curl $URL -s"
+  URL="http://$(minikube ip):30080";
+  watch -n 1 "kubectl get pod -n demo -Lapp/name -L app/instance -L app/version;
+  echo '----';
+  set -x;
+  curl $URL -s"
 
 # 2. Copy namespace and HelmRelease to /manifests
   rm ../manifests/02_helm_release.yaml
@@ -13,7 +17,7 @@
 
 # 4. Push to github and observe Flux logs:
   git push origin master
-  kubectl logs -n flux deployment/flux --since 10m --follow | egrep demo
+  kubectl logs -n flux deployment/flux --since 10m --follow | egrep 'output=".+demo.+(created|configured).*"'
 
 # 5. Remove the Helm Release Object and observe Flux Helm Operator Logs:
   kubectl delete helmrelease demo
