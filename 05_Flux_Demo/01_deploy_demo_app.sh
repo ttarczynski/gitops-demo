8f27ca56558b731f6d318a3a9eb33c36c888626c
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-09-05 12:57:06 +0200

    Changes in 05_Flux_Demo/01_deploy_demo_app.sh

--- 05_Flux_Demo/01_deploy_demo_app.sh
+++ 05_Flux_Demo/01_deploy_demo_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. Monitor results (run in separate terminal):
-watch -n 1 "kubectl get all -n demo"
+watch -n 1 "kubectl get deployment,pod,service,configmap -n demo"
 
 # 2. Copy manifests to /manifests dir
 rsync -av ../01_Demo_App/*.yaml ../manifests/01_Demo_App/
@@ -12,4 +12,4 @@ git commit --message="Add Demo App to GitOps manifests"
 
 # 4. Push to github and observe Flux logs:
 git push origin master
-kubectl logs -n flux deployment/flux --since 10m --follow | egrep '\\n.+demo.+(created|configured)'
+kubectl logs -n flux deployment/flux --since 10m --follow | egrep 'output=".+demo.+(created|configured)"'
