38b0f769d983a7638f3421898be923aa60a24625
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-09-05 17:16:23 +0200

    Change in 05_Flux_Demo/03_delete_namespace.sh

--- 05_Flux_Demo/03_delete_namespace.sh
+++ 05_Flux_Demo/03_delete_namespace.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # 1. Monitor results (run in separate terminal):
-URL=$(minikube service --url -n demo demo | sed 's/\* //');
-watch -n 1 "kubectl get namespace demo; echo; kubectl get pod -n demo -Lapp/name -L app/instance -L app/version;
-echo '----';
-set -x;
-curl $URL -s"
+  URL=$(minikube service --url -n demo demo | sed 's/\* //');
+  watch -n 1 "kubectl get namespace demo; echo; kubectl get pod -n demo -Lapp/name -L app/instance -L app/version;
+  echo '----';
+  set -x;
+  curl $URL -s"
 
 # 2. Delete the demo namespace and observe Flux logs:
-kubectl delete namespaces demo --wait=false
-kubectl logs -n flux deployment/flux --since 10m --follow | egrep 'output=".+demo.+(created|configured).*"'
+  kubectl delete namespaces demo --wait=false
+  kubectl logs -n flux deployment/flux --since 10m --follow | egrep 'output=".+demo.+(created|configured).*"'
