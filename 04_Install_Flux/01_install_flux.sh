00b6f5a12a4e1fa2c4e29d3b2dc2804bd00b837f
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-11-24 20:52:26 +0100

    Upgrade flux chart to v0.15.0

--- 04_Install_Flux/01_install_flux.sh
+++ 04_Install_Flux/01_install_flux.sh
@@ -6,7 +6,7 @@ helm repo add fluxcd https://charts.fluxcd.io
 
 # 2. Install Flux:
 
-chart_version='0.14.1'
+chart_version='0.15.0'
 helm upgrade --install flux \
 --namespace flux \
 --values ./values_flux.yaml \
