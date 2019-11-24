dbed8be32048e1b2cc97518f0551ba28be89e02d
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-11-24 21:50:18 +0100

    Corrections in Helm demo 	modified:   07_Helm_Chart/01_prepare_helm_chart.sh

--- 07_Helm_Chart/01_prepare_helm_chart.sh
+++ 07_Helm_Chart/01_prepare_helm_chart.sh
@@ -2,7 +2,7 @@
 
 # 1. Inspect the helm chart for the demo app
   find charts/demo/ | sort
-  egrep -r 'image:|tag:' charts/
+  egrep -r 'blue:|green:|image:|tag:' charts/
 
   vim charts/demo/templates/03_deployment_blue.yaml -c "/image:"
   vim charts/demo/values.yaml -c "/tag:"
@@ -11,6 +11,6 @@
 # 2. Render chart templates locally and write manifests to /tmp/demo
   helm template ./charts/demo/ --output-dir /tmp
 
-  egrep -r 'image:' /tmp/demo/templates/03_deployment*
+  cat charts/demo/values.yaml
+  egrep -r 'replicas:|image:' /tmp/demo/templates/03_deployment*
   egrep -r -H 'app/instance:' /tmp/demo/templates/04_service*
-  vim /tmp/demo/templates/03_deployment_blue.yaml -c "/image:"
