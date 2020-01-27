ce86b63801e0c9fe7278063c824c0edefd5933e5
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2020-01-27 17:05:27 +0100

    Small fix 	modified:   09_Helm_Operator_Demo/02_change_release_params.sh 	modified:   09_Helm_Operator_Demo/03_change_release_params.sh 	modified:   09_Helm_Operator_Demo/04_delete_release.sh

--- 09_Helm_Operator_Demo/04_delete_release.sh
+++ 09_Helm_Operator_Demo/04_delete_release.sh
@@ -20,7 +20,7 @@
   kubectl logs -n flux deployment/flux --since 10m --follow | egrep 'output="helmrelease.+demo.+deleted"'
 
 # 5. Check Flux Helm Operator Logs:
-  kubectl logs deployment/flux-helm-operator -n flux --since 10m  --follow | egrep -i '(delete|deleted|deleting).+demo'
+  kubectl logs deployment/helm-operator -n flux --since 10m  --follow | egrep -i '(delete|deleted|deleting).+demo'
 
 # 6. See Helm Releases
   helm ls
