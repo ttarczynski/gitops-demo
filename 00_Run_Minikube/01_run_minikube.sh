15ba4d46e683a808dbeaf43fd3c914044c43e0dd
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2020-09-10 12:54:00 +0200

    Update scripts: 01_run_minikube.sh, 01_install_helm.sh 	new file:   00_Run_Minikube/01_run_minikube.sh 	modified:   03_Install_Helm/01_install_helm.sh

---
+++ 00_Run_Minikube/01_run_minikube.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 0. Istall minikube
+#
+#   Follow the instructions at: https://kubernetes.io/docs/tasks/tools/install-minikube/
+
+# 1. Start minikube
+
+minikube start --kubernetes-version v1.18.8
--- 03_Install_Helm/01_install_helm.sh
+++ 03_Install_Helm/01_install_helm.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-# 0. Istall minikube
-#
-#   Follow the instructions at: https://kubernetes.io/docs/tasks/tools/install-minikube/
+# 1. Install the Helm Client
 
-# 1. Start minikube
-
-minikube start
-
-# 2. Install the Helm Client
-
-curl -LO https://git.io/get_helm.sh
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-# 3. Install Tiller
+# 2. Initialize a Helm Chart Repository
 
-kubectl -n kube-system create sa tiller
-
-kubectl create clusterrolebinding tiller-cluster-rule \
-  --clusterrole=cluster-admin \
-  --serviceaccount=kube-system:tiller 
-
-helm init --skip-refresh --upgrade --service-account tiller --history-max 10
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
